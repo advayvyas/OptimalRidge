bb3dfce0ac126aec835c7b57a6e210d3b871a81c
Advay Vyas <advay.vyas@gmail.com>
2026-08-15 17:23:42 -0500

    GenXData

--- alg1.py
+++ alg1.py
@@ -7,13 +7,7 @@ np.set_printoptions(
 )
 
 def algorithm2(N = 100, d = 150):
-    rng = np.random.default_rng(seed=77)
-
-    mu = np.zeros(d)
-    Sigma = np.eye(d)
-    X = rng.multivariate_normal(mean = mu, cov = Sigma, size = N)
-    # print(X)
-
+    
     theta = (rng.standard_normal(size = d))[:, np.newaxis]
     # print(theta)
     # print((X[2]).T @ theta)
@@ -27,4 +21,23 @@ def algorithm2(N = 100, d = 150):
     y = X @ theta + epsilon * z
     # print(y)
 
-algorithm2(N = 4, d = 5)
+def GenXData(N=100, d=120, Sigma=None):
+    if Sigma is None:
+        Sigma = np.eye(d)
+    elif Sigma.shape != (d, d):
+        raise ValueError(f"Sigma must be {d}x{d}, got {Sigma.shape}")
+
+    rng = np.random.default_rng(seed=77)
+
+    mu = np.zeros(d)
+    
+    X = rng.multivariate_normal(mean = mu, cov = Sigma, size = N)
+    # print(X)
+
+    return ((np.eye(N) - 1/N * np.ones(N) @ np.ones(N).T) @ X)
+
+N = 6
+d = 8
+Sigma = np.eye(d)
+GenXData(N, d, Sigma)
+
