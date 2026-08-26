45afa776b41ba8ab0f559d562fe2c31af1913a9d
Advay Vyas <advay.vyas@gmail.com>
2026-08-26 13:00:12 -0500

    Major algorithm 4 updates

--- alg4.py
+++ alg4.py
@@ -9,26 +9,64 @@ np.set_printoptions(
     linewidth=10000
 )
 
+def GlobalMinLambda(S, V, theta, epsilon):
+    lambdas = np.linspace(-1, 10 ** 6, 1000)
+    mse_values = np.array([alg2.mse(S, V, theta, lam, epsilon) for lam in lambdas])
+
+    optimal_mse_value = np.min(mse_values)
+
+    optimal_lambda_idx = np.argmin(mse_values)
+    optimal_lambda = lambdas[optimal_lambda_idx]
+
+    return optimal_lambda, optimal_mse_value
+
 def EvaluateFixedX(N, D, sigma, epsilon, m_theta, m_X, m_y):
     lambda_0 = 1
     delta = 10 ** -4
     p = 1/4
-    
+    rng = np.random.default_rng(seed=77)
+
+    lambda_min = np.zeros(shape = (m_X, m_theta))
+    mse_min = np.zeros(shape = (m_X, m_theta))
+
+    lambda_fixed_pt = np.zeros(shape = (m_X, m_theta))
+    mse_fixed_pt = np.zeros(shape = (m_X, m_theta))
+
+    lambda_sample_fixed_pt = np.zeros(shape = (m_X, m_theta, m_y))
+    mse_sample_fixed_pt = np.zeros(shape = (m_X, m_theta, m_y))
+
+    lambda_signal_noise = np.zeros(shape = (m_X, m_theta, m_y))
+    mse_signal_noise = np.zeros(shape = (m_X, m_theta, m_y))
+
     for i_theta in range(m_theta):
-        # uniform theta
+        theta = rng.standard_normal(size=D)[:, np.newaxis]
+        theta = theta / np.linalg.norm(theta)
+
         for i_X in range(m_X):
             X = alg1.GenXData(N, D, sigma)
-            U, S, Vt = np.linalg.svd(X)
+            U, S, Vt = np.linalg.svd(X) # pylint: disable=unused-variable
             V = V.T
 
-            # min lambda from eq 16
-            # mse from equation 8 and lambda min
-            # lambda fixed point, mse using indexing with ModelOptReg
+            lambda_min[i_X, i_theta], mse_min[i_X, i_theta] = GlobalMinLambda(S, V, theta, epsilon)
+            lambda_fixed_pt[i_X, i_theta], mse_fixed_pt[i_X, i_theta] = alg2.ModelOptReg(S,
+                V, theta, epsilon, lambda_0, delta)
 
             for i_y in range(m_y):
-                y = alg1.GenYData(X, theta, epsilon)
-                # sample fixed point lambda from alg3.SampleOptReg
-                # mse from equation 8 and lambda and indices
-                # lambda signal to noise, mse from 4.2.3
+                y = alg1.GenYData(X, N, theta, epsilon)
 
-    # return BC-a bootstrapped 95% CI for lambda, mse values
+                lambda_sample_fixed_pt[i_X, i_theta, i_y], mse_sample_fixed_pt[i_X,
+                    i_theta, i_y] = alg3.SampleOptReg(X, y, lambda_0, p, delta)
+
+                # TODO lambda signal to noise, mse from 4.2.3
+
+    # TODO return BC-a bootstrapped 95% CI for lambda, mse values
+
+if __name__ == "__main__":
+    # trial
+    N_TRIAL = 6
+    D_TRIAL = 8
+    sigma_trial = np.eye(D_TRIAL)
+    M_THETA = 100
+    M_X = 50
+    M_Y = 50
+    EPSILON = 6.767
