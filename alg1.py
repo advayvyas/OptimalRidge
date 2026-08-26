0aa866a66e7d6471b39144db4bab5afc31bc198f
Advay Vyas <advay.vyas@gmail.com>
2026-08-26 13:12:29 -0500

    Alg 1 method docstrings

--- alg1.py
+++ alg1.py
@@ -7,6 +7,17 @@ np.set_printoptions(
 )
 
 def GenXData(N, D, sigma):
+    """
+    Generates a random mean-centered X from a multivariate normal distribution.
+
+    Args:
+        N: the number of observations
+        D: the number of parameters
+        sigma: the covariance matrix of the data (X)
+
+    Returns:
+        A matrix X of the shape NxD (N rows, D columns).
+    """
     rng = np.random.default_rng(seed=77)
 
     mu = np.zeros(D)
@@ -14,6 +25,18 @@ def GenXData(N, D, sigma):
     return X - X.mean(axis=0)
 
 def GenYData(X, N, theta, epsilon):
+    """
+    Generates a random mean-centered y from a standard normal distribution.
+
+    Args:
+        N: the number of observations
+        D: the number of parameters
+        theta: the true parameter vector
+        epsilon: the noise amplitude
+
+    Returns:
+        A column vector y of the shape Nx1 (N rows, 1 column).
+    """
     rng = np.random.default_rng(seed=77)
 
     z = (rng.standard_normal(size = N))[:, np.newaxis]
@@ -21,6 +44,19 @@ def GenYData(X, N, theta, epsilon):
     return y - y.mean(axis=0)
 
 def GenData(sigma, theta, N, D, epsilon):
+    """
+    Generates random mean-centered X and y from GenXData, GenYData.
+
+    Args:
+        sigma: the covariance matrix of the data (X)
+        theta: the true parameter vector
+        N: the number of observations
+        D: the number of parameters
+        epsilon: the noise amplitude
+
+    Returns:
+        A matrix X (NxD) and vector y (Nx1).
+    """
     X = GenXData(N, D, sigma)
     y = GenYData(X, N, theta, epsilon)
     return X, y
