5aa437a6e5d070aee385ac740ce6b77b263f9bb5
Advay Vyas <advay.vyas@gmail.com>
2026-08-26 15:00:13 -0500

    signal noise lambda

--- alg4.py
+++ alg4.py
@@ -10,6 +10,21 @@ np.set_printoptions(
 )
 
 def GlobalMinLambda(S, V, theta, epsilon):
+    """
+    Finds the globally optimal ridge regression parameter 
+        that minimizes the mean squared error (MSE).
+
+    Args:
+        S: the singular value matrix of X from its SVD decomposition, 
+            with singular values on the main diagonal.
+        V: the matrix of right eigenvectors of X from its SVD decomposition.
+        theta: the true parameter vector.
+        lambda_: the ridge regression parameter.
+
+    Returns:
+        The optimal parameter and its corresponding MSE value as a tuple.
+
+    """
     lambdas = np.linspace(-1, 10 ** 6, 1000)
     mse_values = np.array([alg2.mse(S, V, theta, lam, epsilon) for lam in lambdas])
 
@@ -19,6 +34,20 @@ def GlobalMinLambda(S, V, theta, epsilon):
     optimal_lambda = lambdas[optimal_lambda_idx]
 
     return optimal_lambda, optimal_mse_value
+
+def SignalNoiseLambda(X, y, lambda_0, p):
+    U, S, Vt = np.linalg.svd(X)
+    V = Vt.T
+
+    N = S.shape[0]
+    D = S.shape[1]
+
+    theta_hat = V @ np.linalg.pinv(S + (lambda_0 @ np.eye(N, D))) @ (U.T) @ y
+    y_hat = X @ theta_hat
+
+    epsilon_hat = alg3.EpsilonEstimate(X, y, y_hat, p, lambda_0 = 1)
+
+    return (D * (epsilon_hat ** 2)) / (np.sum(np.square(theta_hat)))
 
 def EvaluateFixedX(N, D, sigma, epsilon, m_theta, m_X, m_y):
     lambda_0 = 1
@@ -47,7 +76,8 @@ def EvaluateFixedX(N, D, sigma, epsilon, m_theta, m_X, m_y):
             U, S, Vt = np.linalg.svd(X) # pylint: disable=unused-variable
             V = V.T
 
-            lambda_min[i_X, i_theta], mse_min[i_X, i_theta] = GlobalMinLambda(S, V, theta, epsilon)
+            lambda_min[i_X, i_theta], mse_min[i_X,
+                i_theta] = GlobalMinLambda(S, V, theta, epsilon)
             lambda_fixed_pt[i_X, i_theta], mse_fixed_pt[i_X, i_theta] = alg2.ModelOptReg(S,
                 V, theta, epsilon, lambda_0, delta)
 
@@ -57,7 +87,8 @@ def EvaluateFixedX(N, D, sigma, epsilon, m_theta, m_X, m_y):
                 lambda_sample_fixed_pt[i_X, i_theta, i_y], mse_sample_fixed_pt[i_X,
                     i_theta, i_y] = alg3.SampleOptReg(X, y, lambda_0, p, delta)
 
-                # TODO lambda signal to noise, mse from 4.2.3
+                lambda_signal_noise[i_X, i_theta, i_y], mse_signal_noise[i_X,
+                    i_theta, i_y] = SignalNoiseLambda(X, y, lambda_0, p)
 
     # TODO return BC-a bootstrapped 95% CI for lambda, mse values
 
