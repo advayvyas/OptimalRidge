7770c10b56ba5774da916d9b21d2a5b236a04895
Advay Vyas <advay.vyas@gmail.com>
2026-08-26 01:26:02 -0500

    Algorithm 3
    - epsilon estimate is complete!!!

--- alg3.py
+++ alg3.py
@@ -9,13 +9,24 @@ np.set_printoptions(
 
 rng = np.random.default_rng(seed=77)
 
-def EpsilonEstimate(S, theta_hat, p): # equation 14, 15
-    if S.shape[0] > S.shape[1]: # if N > D
-        print("underparam case")
-    else:
-        print("overparam case")
+def EpsilonEstimate(X, y, y_hat, p, lambda_0):
+    U, S, Vt = np.linalg.svd(X) # pylint: disable=unused-variable
 
-    return 2
+    N = S.shape[0]
+    D = S.shape[1]
+    min_D_N = min(S.shape)
+    sigma = np.diag(S)[:min_D_N]
+
+    r_p = np.sum(((sigma ** 2) / (sigma ** 2 + lambda_0)) ** p)
+
+    if N > D:
+        epsilon_hat = np.sqrt((1/(N-1) * np.sum((y_hat - y) ** 2)) / (1 - r_p/N)) # equation 14
+    else: # manual 2-fold CV by series indices
+        h = N // 2
+        mse_oos = 1/N [np.sum((y_hat[h:N] - y[0:h]) ** 2) + np.sum((y_hat[0:h] - y[h:N]) ** 2)]
+        epsilon_hat = np.sqrt((N/(N-1) * mse_oos) / (1 - r_p/N))
+
+    return epsilon_hat
 
 def SampleOptReg(X, y, lambda_0, p, delta):
     U, S, Vt = np.linalg.svd(X)
@@ -25,7 +36,9 @@ def SampleOptReg(X, y, lambda_0, p, delta):
     D = S.shape[1]
 
     theta_hat = V @ np.linalg.pinv(S + (lambda_0 @ np.eye(N, D))) @ (U.T) @ y
-    epsilon_hat = EpsilonEstimate(S, theta_hat, p)
+    y_hat = X @ theta_hat
+
+    epsilon_hat = EpsilonEstimate(X, y, y_hat, p, lambda_0 = 1)
 
     return alg2.ModelOptReg(S, V, theta_hat, epsilon_hat, lambda_0, delta)
 
